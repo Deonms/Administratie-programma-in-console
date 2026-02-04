1c8c79a4e7f82ea21c13847de43e3202a4d184b1
DeonsGaming <Deon.mob@gmail.com>
2026-02-04 09:53:11 +0100

    the start of it

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Administratie_programma_in_console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wat wil je doen?");
+            Console.WriteLine("1. Voeg Klant toe");
+            Console.WriteLine("2. Bewerk Klant");
+            Console.WriteLine("3. Zoek een Klant");
+            Console.WriteLine("4. Verwijder Klant");
+            Console.WriteLine("5. Sluit Programma af");
+        }
+    }
+}
